ce3ca55c56d19d65a4bfd91703c97fa38a98e2f3
Mayank Modi <57084536+modi0501@users.noreply.github.com>
2021-12-08 18:14:49 +0530

    Update Assignment_9.js

--- Assignment_9.js
+++ Assignment_9.js
@@ -1,33 +1,18 @@
 // 1. Add the list of employees in a json file and import it in below file to use it. Add unique ids for each employee.
-
-const obj = require('./employeeList.json');
-
-obj.forEach((e , index) => {
-    e["id"] = index+1;
+const employeeList = require("./employeeList.json");
+employeeList.forEach((e, index) => {
+  e["id"] = index + 1;
 });
-
 
 // 2. Add a function to return list of employees that contains "am" in his name. Add it in separate file and import it to call in another file.
-const fun = require('./nameSearch');
-const newArray = fun.nameSearch(obj);
+const searchFunction = require("./nameSearch");
+const newArray = searchFunction.nameSearch(employeeList);
 console.log(newArray);
 
-
 // 3. Take another paramter as salary and keep it above 500000 in numbers in json and print it upto one decimal place only with unit as Lac
-let result = obj.map((e) => {
-    let l = e.salary / 100000;
-    l = l.toFixed(1);
-    return l.toString() + " Lac";
+const salaryInLacs = employeeList.map((e) => {
+  let temp = e.salary / 100000;
+  temp = temp.toFixed(1);
+  return temp.toString() + " Lac";
 });
-
-console.log(result);
-
-
-
-
-
-
-
-
-
-
+console.log(salaryInLacs);
